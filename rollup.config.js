6f41d18e940ea8919a34cdc10e1b6791eb14ed6e
jongseok-lee <jongseok.lee@linecorp.com>
2022-04-29 11:29:58 +0900

    fix: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import babel from '@rollup/plugin-babel';
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
@@ -25,12 +26,18 @@ export default [
         ],
         plugins: [
             peerDepsExternal(),
+            babel({
+                babelHelpers: 'bundled',
+                exclude: 'node_modules/**',
+                presets: ['@babel/preset-env','@babel/preset-react']
+            }),
             resolve(),
             commonjs(),
             typescript({tsconfig: "./tsconfig.json"}),
             postcss(),
             terser(),
         ],
+        external: Object.keys(packageJson.peerDependencies)
     },
     {
         input: "dist/esm/types/index.d.ts",
